91535063a10a5d51e259a1968afabef7290069c4
Dylan Rodriguez <dylanrod24@users.noreply.github.com>
2018-09-20 19:19:57 -0700

    put a show/hide on descriptions

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -9,17 +9,24 @@ import { HttpService } from './http.service';
 export class AppComponent implements OnInit {
   title = 'API';
   tasks = [];
+  isHidden = true;
 
   constructor(private _httpService: HttpService){ // '_httpService is the name and HttpService  is the type
   }
   ngOnInit(){
-      this.getTasksFromService() // Having the component invoke getTasks from service.ts
   }
  getTasksFromService() {
      let observable = this._httpService.getTasks();
      observable.subscribe((data: any) => {
          console.log("Got our data!", data)
          this.tasks = data.data;
+     })
+ }
+ onButtonClick(): void {
+    this.getTasksFromService(); // Having the component invoke getTasks from service.ts
+ }
+ onButtonClickShow(): void {
+     this.isHidden = false;
      })
  }
 }
